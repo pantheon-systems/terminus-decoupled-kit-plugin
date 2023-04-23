d554322e53d7cd900f81ec1c5df4607043a4fde6
Brian Perry <brian.perry@pantheon.io>
2023-04-23 15:32:16 -0500

    Revert "Install back-end and run create-pantheon-decoupled-kit based on selected CMS"
    This reverts commit 1a36d0e667d6f8ab696df59f9cbbe920d90f4fcb.

--- next-drupal-starter/components/page-header.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './page-header.module.css';
-
-export default function PageHeader({ title }) {
-	return (
-		<header className={styles.header}>
-			<h1>{title}</h1>
-		</header>
-	);
-}
